79f4bdb6192b2d01f19c3942f1b88bdd7ebc052b
Philipp Borgers <borgers@mi.fu-berlin.de>
2014-03-27 12:02:12 +0100

    draw pixel on mouse up

--- src/gui.c
+++ src/gui.c
@@ -6,9 +6,16 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+void putPixel(SDL_Renderer* renderer, int x, int y, int alpha) {
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, alpha);
+  SDL_RenderDrawPoint(renderer, x, y);
+}
+
 int main(int argc, char *argv) {
 
   SDL_Window* window = NULL;
+  SDL_Renderer* renderer = NULL;
+  SDL_Texture* texture = NULL;
   SDL_Surface* screenSurface = NULL;
   SDL_Event event;
 
@@ -28,14 +35,46 @@ int main(int argc, char *argv) {
     if(window == NULL) {
       printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
     } else {
-      screenSurface = SDL_GetWindowSurface(window);
-      SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-      SDL_UpdateWindowSurface(window);
+      renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE);
+
+      SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+
+      texture = SDL_CreateTexture(
+          renderer,
+          SDL_PIXELFORMAT_ARGB8888,
+          SDL_TEXTUREACCESS_TARGET,
+          SCREEN_WIDTH,
+          SCREEN_HEIGHT
+          );
+
+      //clear screen
+      SDL_SetRenderTarget(renderer, texture);
+      SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+      SDL_RenderClear(renderer);
+      SDL_SetRenderTarget(renderer, NULL);
+      SDL_RenderCopy(renderer, texture, NULL, NULL);
+      SDL_RenderPresent(renderer);
 
       while(!quit) {
         while(SDL_PollEvent(&event) != 0) {
-          if(event.type == SDL_QUIT) {
-            quit = 1;
+          switch(event.type) {
+            case SDL_QUIT:
+                quit = 1;
+                break;
+            case SDL_MOUSEBUTTONUP:
+                printf("Mouse up event\n");
+                SDL_SetRenderTarget(renderer, texture);
+                putPixel(renderer, event.motion.x, event.motion.y, 255);
+                SDL_SetRenderTarget(renderer, NULL);
+                SDL_RenderCopy(renderer, texture, NULL, NULL);
+                SDL_RenderPresent(renderer);
+                break;
+            case SDL_MOUSEBUTTONDOWN:
+                printf("Mouse down event\n");
+                break;
+            case SDL_MOUSEMOTION:
+                printf("Mouse motion event\n");
+                break;
           }
         }
       }
