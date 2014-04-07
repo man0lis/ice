10afc1276a13cb362a284ccda1d07b31b5824dfa
Philipp Borgers <borgers@mi.fu-berlin.de>
2014-04-07 17:56:44 +0200

    use SDL_FillRect to draw pixels (fixes segfault)

--- src/fluids.c
+++ src/fluids.c
@@ -411,11 +411,12 @@ Uint32 get_pixel32( SDL_Surface *surface, int x, int y ) {
 }
 
 void put_pixel32( SDL_Surface *surface, int x, int y, Uint32 pixel ) {
-    //Convert the pixels to 32 bit
-    Uint32 *pixels = (Uint32 *)surface->pixels;
-    
-    //Set the pixel
-    pixels[ ( y * surface->w ) + x ] = pixel;
+    SDL_Rect dstrect;
+    dstrect.x = x;
+    dstrect.y = y;
+    dstrect.w = 1;
+    dstrect.h = 1;
+    SDL_FillRect(surface, &dstrect, pixel);
 }
 
 int main(int argc, char *argv) {
